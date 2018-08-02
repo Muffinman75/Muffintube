0af5c7cce3a9b30ded0d5e7c63d6096fe8186a23
Manoj Modhwadia <manojmodhwadia@outlook.com>
2018-08-02 19:04:43 +0100

    switched to .ajax instead of .getJSON

--- index.js
+++ index.js
@@ -5,43 +5,43 @@ const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search?';
 let resultNumber = 0;
 
 function getDataFromApi(inputText, callback) {
-	// Take the value passed from handleSubmit and use 
-	// it to find the data for the videos related to 
+	// Take the value passed from handleSubmit and use
+	// it to find the data for the videos related to
 	// the value
 	const paramsObject = {
 		url: YOUTUBE_SEARCH_URL,
-		data: { 
+		data: {
 			part: 'snippet',
 			q: inputText,
 		    maxResults: 4,
 			key: 'AIzaSyAw1hPcxvy1hwfZ8fTP-zOHaPzxVuqKFDI',
 			type: 'video'
 		},
-		// type: 'GET',
-		// dataType: 'json',
+	  type: 'GET',
+		dataType: 'json',
 		success: callback,
 		error: function(error) {
-			console.log(error);
+			console.log('error');
 		}
 	};
 
-	$.getJSON(paramsObject);
-}	
+	$.ajax(paramsObject);
+}
 
 function renderSearchResults(item) {
 	// Return the template string with
-	// all thumbnails and captions ready to 
+	// all thumbnails and captions ready to
 	// inject into the results page div
 	console.log(`'renderSearchResults' ran`);
 	incrementResultNumber();
 	return`
-		<h2>Search result ${resultNumber}</h2> 
+		<h2>Search result ${resultNumber}</h2>
 		<a href="https://www.youtube.com/watch?v=${item.id.videoId}" target="_blank"><img src=${item.snippet.thumbnails.default.url} alt="screenshot from ${item.snippet.title}">Small Image</a>
 		<h2>Search result ${resultNumber}</h2>
 		<a href="https://www.youtube.com/watch?v=${item.id.videoId}" target="_blank"><img src=${item.snippet.thumbnails.medium.url} alt="screenshot from ${item.snippet.title}">Medium Image</a>
 		<h2>Search result ${resultNumber}</h2>
 		<a href="https://www.youtube.com/watch?v=${item.id.videoId}" target="_blank"><img src=${item.snippet.thumbnails.high.url} alt="screenshot from ${item.snippet.title}">Large Image</a>
-		`; 
+		`;
 }
 
 function incrementResultNumber() {
@@ -49,12 +49,12 @@ function incrementResultNumber() {
 }
 
 function displayResultsPage(data) {
-	// Inject the HTML into the results page to 
+	// Inject the HTML into the results page to
 	// display in the DOM
 	const listOfScreenshots = data.items.map((item, index) => renderSearchResults(item));
 	console.log(listOfScreenshots);
 	console.log(`'displayResultsPage' ran`);
-	$('.js-results-page').prop('hidden', false);		
+	$('.js-results-page').prop('hidden', false);
 	$('.js-results-page').html(listOfScreenshots);
 }
 
